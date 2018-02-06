73a953efdfeb075df321323066aa65de95baf414
Robert Desjardins <roberttdesjardins@gmail.com>
2018-02-06 15:00:17 -0800

    Added calculate button functionality, clear calculator button

--- window-shopper/Views/CurrencyTxtField.swift
+++ window-shopper/Views/CurrencyTxtField.swift
@@ -30,7 +30,7 @@ class CurrencyTxtField: UITextField {
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)])
             attributedPlaceholder = place
-            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         }
 
     }
